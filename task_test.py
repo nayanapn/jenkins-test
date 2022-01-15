7ea1f2115ba92e585760dd43c0b525c737794e2c
nayanapn <56036382+nayanapn@users.noreply.github.com>
2022-01-15 23:19:52 +0530

    Add files via upload

--- task_test.py
+++ task_test.py
@@ -1,2 +1,6 @@
+checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/nayanapn/jenkins-test']]])
 print("HIIIIIII, HELLLOOOOOO")
 print("Happy sankranthiiii")
+
+print("happpuyyduere")
+
